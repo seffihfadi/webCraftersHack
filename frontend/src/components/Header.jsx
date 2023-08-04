b309eb41271243b53ddf4f5c1fee0a3b89fc7c64
zakaryaalgeria <zz194451@gmail.com>
2023-08-04 15:55:33 +0100

    change the home page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import UnopDropdown from 'unop-react-dropdown'
 
 const Header = () => {
+  const handler = ()=>{  }
   return (
-    <div>Header</div>
+    <div className='flex justify-between h-16 items-center'>
+        <Link to='/' > Logo </Link>
+        <div className='hidden md:flex gap-12'>
+            <Link to='/home' >Home</Link>
+            <Link to='/service'> Service</Link>
+            <Link to='/about' > About us</Link>
+            <Link to='/contact' > Contact </Link>
+        </div>
+        <div className='md:hidden'>
+            <UnopDropdown
+            onAppear={handler}
+            onDisappearStart={handler}
+            trigger={<button className="AnimatedDropdownButton flex gap-6 items-center">
+               <i className='material-symbols-rounded' >menu</i>
+            </button>}
+            delay={300}
+            align="CENTER"
+            hover
+            >
+            <div className='drop-down'>
+                <div> <Link to='/home' >Home</Link></div>
+                <div><Link to='/service'> Service</Link></div>
+                <div><Link to='/about' > About us</Link></div>
+                <div><Link to='/contact' > Contact </Link></div>
+            </div>
+            </UnopDropdown>
+        </div>
+        
+    </div>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,23 +2,26 @@
 //import { NavBar } from '../components'
 import { Header } from '../components'
 import { Link } from 'react-router-dom'
+import background from '../assets/img.png'
 const Home = () => {
+  const backgroundStyles = {
+    backgroundImage: `url(${background})`, // Use template literal to insert the image URL
+  };
+
+
   return (
-    <div className='md:px-[140px] px-20 '>
-        <Header />
-        <p className='w-[300px ] font-bold md:w-[600px] leading-10 mt-[50px] md:mt-[140px]'>
+    <div className='md:px-[140px] px-20 pb-10 text-white' style={backgroundStyles}>
+        <Header/>
+        <p className='w-[300px ] font-bold md:w-[600px] leading-10 mt-[100px] md:mt-[140px]'>
         Welcome to<span style={{ color: 'rgba(133, 208, 39, 1)' }} className='text-3xl' > Agrouland</span>,
         where farmers and providers unite.
         Cultivating connections, harvesting success, day and night.
         Grow your network, sow the seeds of progress, far and wide.
         Join us now, and let prosperity in agriculture abide.
         </p>
-        <div className='relative my-[50px] md:my-[120px]'>
-            <Link to='/signin' className="btn-primary">Get started</Link>
-        </div>
-
-        <span className="material-symbols-rounded">home</span>
-        <button className="btn-primary">Get started</button>
+        <div className='my-[50px] md:my-[120px]  flex justify-end'>
+          <Link to='/signin' className="btn-primary">Get started</Link>
+        </div> 
     </div>
   )
 }
